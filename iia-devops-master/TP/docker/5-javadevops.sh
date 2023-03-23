d3f9c2eb893d57d5d85d85b91b1ca9daee6b2622
Yaznarb <104253037+Yaznarb@users.noreply.github.com>
2023-03-23 16:34:28 +0100

    Delete iia-devops-master directory

--- iia-devops-master/TP/docker/5-javadevops.sh
+++
@@ -1,5 +0,0 @@
-# Fabriquer l'image
-docker build -t javadevops:latest .
-
-# Démarrer un nouveau container à partir de l'image
-docker run -d --name java --network devops -p 5600:8080 javadevops
--- iia-devops-master/TP/docker/6-sonarqube.sh
+++
@@ -1,2 +0,0 @@
-# Pour créer le container sonarqube
-docker run -d --name sonar -p 9000:9000 --network devops --ip 172.20.0.30 sonarqube:latest
